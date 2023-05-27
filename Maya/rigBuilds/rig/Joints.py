59b85e06aa110d7901b483ceb9b1048b28541336
kimoo <kimoon.t.song@gmail.com>
2023-05-26 23:52:56 -0400

    finished createJointChain

--- Maya/rigBuilds/rig/Joints.py
+++ Maya/rigBuilds/rig/Joints.py
@@ -1,11 +1,11 @@
 import pymel.core as pm
-import attr
+from rigBuilds import attribute
 import importlib as imp
 
-def createJointChain(guideList=['C_spline%s_GDE' % i for i in range(4)],
-                     parent = None,
+def createJointChain(guideList=['C_spine%s_GDE' % i for i in range(4)],
+                     parent='SKELE',
                      primaryAxis='xyz',
-                     secondayAxis='z',
+                     secondaryAxisOrient = 'yup',
                      orientJointEnd=True,
                      tag=True,
                      ):
@@ -13,39 +13,51 @@ def createJointChain(guideList=['C_spline%s_GDE' % i for i in range(4)],
     FUNCTION:      createJointChain()
     DESCRIPTION:   Creates Joint Chains for rig setup
     USAGE:         createJointChain(name='ACME', side='C', shape='acme', joints=['joint1'], scale = 3,
-    RETURN:        list of guides ex []
+    RETURN:        list of joint ex []
     AUTHOR:        Tony K Song
     DATE:          05/25/2023
     Version        1.0.0
 
     guideList       :   Names of locGuides
-    parent          :   parent
+    parent          :   to parent the entire chain
     primaryAxis     :   xyz as default,
     secondayAxis    :   axis to point up to
     orientJointEnd  :   to orient the end joint of the chain with alignment the chain
-
+    tag             :   to tag current made joints
     '''
-
+    JntList = []
     otherJntList = []
     otherJntList.append(parent)
     jointList = []
-    # create joints
     for guides in guideList:
+        # delete existing chain from scene
+        if pm.objExists(guides.replace('_GDE', '_JNT')):
+            pm.delete(guides.replace('_GDE', '_JNT'))
+        # create joints
         gde = pm.PyNode(guides)
-        jnts = pm.joint(n=guides,
+        jnts = pm.joint(
+                        n=guides.replace('_GDE', '_JNT'),
                         p=gde.translate.get(),
                         o=gde.rotate.get(),
-                        oj= primaryAxis,
-                        sao=secondayAxis,
                         )
+
         jointList.append(jnts)
-        # parent
-        pm.parent(jnts,otherJntList[-1])
+        JntList.append(jnts)
+        # parent - create the joint chian
+        pm.parent(jnts, otherJntList[-1])
+        otherJntList.append(jnts)
         # tag
         if tag:
-            pass
-        # orientJointEnd to last joint
-        if orientJointEnd:
-            pass
+            attribute.createTags(node=jnts, tagName='joint', tagValue='JNT')
 
-    return guideList
+    # Orient the joint chain
+    pm.select(JntList)
+    pm.joint(e=True, oj=primaryAxis,
+             secondaryAxisOrient=secondaryAxisOrient,
+             ch=True, zso=True)
+    # Orient the end joint
+    pm.select(JntList[-1])
+    pm.joint(e=True, oj='none',ch=True, zso=orientJointEnd)
+    pm.select(JntList[-1], deselect=True)
+
+    return JntList
