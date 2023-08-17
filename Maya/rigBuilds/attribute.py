a2dfb200a04c6600a57c023f516b5e20efb5b9db
kimoo <kimoon.t.song@gmail.com>
2023-08-17 10:54:00 -0400

    added selectTags def and toggleAttributeLock def

--- Maya/rigBuilds/attribute.py
+++ Maya/rigBuilds/attribute.py
@@ -1,14 +1,13 @@
 import pymel.core as pm
 
 """
-__________________________________________________
 FUNCTION:      createTags
 DESCRIPTION:   crates tags for easy selection
 USAGE:         tagged_objs = pm.ls('*.*myTag' == 'myTagValue')
 RETURN:        None
 REQUIRES:      None
 Version        1.0.0
-__________________________________________________
+Examples:      
 """
 
 def createTags(node='C_joint0_JNT', tagName='myTag', tagValue='myTagValue'):
@@ -20,5 +19,65 @@ def createTags(node='C_joint0_JNT', tagName='myTag', tagValue='myTagValue'):
         obj.addAttr(tagName, dataType='string')
         # set the value of the attribute
         obj.attr(tagName).set(tagValue)
+
     else:
         pm.warning('Object ' + node + ' does not exist.')
+
+"""
+FUNCTION:      selectTags
+DESCRIPTION:   select tags for easy selection
+USAGE:         tagged_objs = pm.ls('*.*myTag' == 'myTagValue')
+RETURN:        List of of oject names as 
+REQUIRES:      None
+Version        1.0.0
+"""
+
+def selectTags(tagName='SKIN'):
+    # list all objects with the specified tag attribute
+    taggedObjs = []
+    for node in pm.ls(type='transform'):
+        if node.hasAttr(tagName):
+            taggedObjs.append(node)
+    if not taggedObjs:
+        pm.warning('No objects found with the tag attribute: {}'.format(tagName))
+    pm.select(taggedObjs)
+
+    # return listed name as string
+    strNameList = []
+    for bah in taggedObjs:
+        name = bah.name()
+        strNameList.append(name)
+    return strNameList
+
+
+"""
+FUNCTION:      toggleAttributeLock
+DESCRIPTION:   lock and unlocks 
+USAGE:         
+RETURN:        None
+REQUIRES:      None
+Version        1.0.0
+"""
+def toggleAttributeLock(node='C_Control0_CNT', unlock=False):
+    if not pm.objExists(node):
+        return
+    # If 'unlock' is False, we are in lock mode.
+    if not unlock:
+        attrs = ['visibility'] if pm.nodeType(node) == 'mesh' else pm.listAttr(node, k=True)
+
+        if attrs:
+            for attr in attrs:
+                try:
+                    pm.setAttr(f'{node}.{attr}', l=True, k=False)
+                except:
+                    continue
+    # If 'unlock' is True, we are in unlock mode.
+    else:
+        attrs = pm.listAttr(node, l=1)
+
+        if attrs:
+            for attr in attrs:
+                try:
+                    pm.setAttr(f'{node}.{attr}', l=False, k=True)
+                except:
+                    continue
