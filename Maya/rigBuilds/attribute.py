77dbadad0d8cadc3f5686c91e693e91d42fece95
rusherman00 <kimoon.t.song@gmail.com>
2023-10-26 20:21:18 -0400

    added checkAttributeExists

--- Maya/rigBuilds/attribute.py
+++ Maya/rigBuilds/attribute.py
@@ -1,17 +1,16 @@
 import pymel.core as pm
 
-"""
-FUNCTION:      createTags
-DESCRIPTION:   crates tags for easy selection
-USAGE:         tagged_objs = pm.ls('*.*myTag' == 'myTagValue')
-RETURN:        None
-REQUIRES:      None
-Version        1.0.0
-Examples:      SKIN     :   skinweights
-               JOINTS   :   joints
-"""
-
 def createTags(node='C_joint0_JNT', tagName='myTag', tagValue='myTagValue'):
+    """
+    FUNCTION:      createTags
+    DESCRIPTION:   crates tags for easy selection
+    USAGE:         tagged_objs = pm.ls('*.*myTag' == 'myTagValue')
+    RETURN:        None
+    REQUIRES:      None
+    Version        1.0.0
+    Examples:      SKIN     :   skinweights
+                   JOINTS   :   joints
+    """
     # check if the object exists
     if pm.objExists(node):
         # get the PyNode object
@@ -24,16 +23,15 @@ def createTags(node='C_joint0_JNT', tagName='myTag', tagValue='myTagValue'):
     else:
         pm.warning('Object ' + node + ' does not exist.')
 
-"""
-FUNCTION:      selectTags
-DESCRIPTION:   select tags for easy selection
-USAGE:         tagged_objs = pm.ls('*.*myTag' == 'myTagValue')
-RETURN:        List of of oject names as 
-REQUIRES:      None
-Version        1.0.0
-"""
-
 def selectTags(tagName='SKIN'):
+    """
+    FUNCTION:      selectTags
+    DESCRIPTION:   select tags for easy selection
+    USAGE:         tagged_objs = pm.ls('*.*myTag' == 'myTagValue')
+    RETURN:        List of of oject names as
+    REQUIRES:      None
+    Version        1.0.0
+    """
     # list all objects with the specified tag attribute
     taggedObjs = []
     for node in pm.ls(type='transform'):
@@ -51,15 +49,16 @@ def selectTags(tagName='SKIN'):
     return strNameList
 
 
-"""
-FUNCTION:      toggleAttributeLock
-DESCRIPTION:   lock and unlocks 
-USAGE:         
-RETURN:        None
-REQUIRES:      None
-Version        1.0.0
-"""
+
 def toggleAttributeLock(node='C_Control0_CNT', unlock=False):
+    """
+    FUNCTION:      toggleAttributeLock
+    DESCRIPTION:   lock and unlocks
+    USAGE:         toggleAttributeLock(node='C_Control0_CNT', unlock=False)
+    RETURN:        None
+    REQUIRES:      None
+    Version        1.0.0
+    """
     if not pm.objExists(node):
         return
     # If 'unlock' is False, we are in lock mode.
@@ -82,3 +81,25 @@ def toggleAttributeLock(node='C_Control0_CNT', unlock=False):
                     pm.setAttr(f'{node}.{attr}', l=False, k=True)
                 except:
                     continue
+
+def checkAttributeExists(objectName, attributeName):
+    """
+    FUNCTION:      checkAttributeExists(object_name, attribute_name)
+    DESCRIPTION:   Check if a specific attribute exists on a given object in Maya.
+    USAGE:
+    RETURN:        bool
+    REQUIRES:      None
+    Version        1.0.0
+    """
+    # Check if the object exists
+    if pm.objExists(objectName):
+        # Get the PyNode for the object
+        obj = pm.PyNode(objectName)
+
+        # Check if the attribute exists
+        if obj.hasAttr(attributeName):
+            return True, print('Yes, the attribute exists.')
+        else:
+            return False, print('No, the attribute does not exist.')
+    else:
+        print(f"The object '{objectName}' does not exist in the scene.")
