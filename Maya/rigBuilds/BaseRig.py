72729a2eca49442c5fc14e1e82f99e1687b204a4
rusherman00 <kimoon.t.song@gmail.com>
2023-11-07 19:53:52 -0500

    fix rigbase2

--- Maya/rigBuilds/BaseRig.py
+++ Maya/rigBuilds/BaseRig.py
@@ -5,81 +5,82 @@ from rigBuilds import ControlCurves
 
 imp.reload(ControlCurves)
 
-class baseRig():
-    def __init__(self,
-                 name='Base',
-                 size=1,
-                 groups={
-                         'RIG': ['C_main_GRP', 'C_global_CTL', 'C_globalGimbal_CTL', 'C_local_GRP'],
-                         'GEO': ['C_main1_GRP', 'C_mainBuffer0_GRP'],
-                         'SKELE': ['C_main2_GRP', 'C_mainBuffer2_GRP']
-                         },
-                # TODO
-                #  colour: 1,
-                #  upaxis:'y',
-                 ):
-        """
-
-        :type children: list type
-        """
-        self.name = name
-        self.size = size
-        self.groups = groups
-
-        # TODO
-        # self.colour = colour
-        # self.upaxis = upaxis
-
-        self.fullName = 'C_' + name + '_GRP'
-        # procs
-        self.__create()
-
-    # def __instancecheck__(self, instance):
-    #     if pm.objExist(self.name):
-    #         logging.warning(self.finalFullName + "same name exists in scene.")
-
-    def __createStructure(self):
-        # create the main group
-        mainGrp = pm.group(em=True, n=self.name)
-        # create the child groups
-        for stuff in self.groups.keys():
-            grp = pm.group(em=True, n=stuff, p=mainGrp)
-
-        # get values and make them in to groups
-        for key, val in self.groups.items():
-            nextList = []
-            num = 0
-            for lst in val:
-                if lst == val[0]:
-                    pm.group(em=True, n=lst, p=key)
-                    nextList.append(lst)
-                else:
-                    if "CTL" in lst:
-                        val = lst.replace('C_', '').replace('_CTL', '')
-                        control = ControlCurves.controlCurves(name=val,
-                                                              side='C',
-                                                              shape='god',
-                                                              num=0,
-                                                              rotate=[0, 0, 0],
-                                                              scale=self.size - num,
-                                                              parent=[nextList[num]],
-                                                              parentOrConst='parent',
-                                                              hook=nextList[-1],
-                                                              adjGrpNumber=1,
-                                                              tag=True
-                                                              )
-                        nextList.append(control.getInstancesList()[-1])
-                        num += 1
-                    else:
-                        bah = pm.group(em=True, n=lst, p=nextList[num])
-                        nextList.append(bah)
-                        num += 1
-
-    # Exacute process
-    def __create(self):
-        # Step 1
-        self.__createStructure()
-        # self.__createGrpsandSubGrps()
+# class baseRig():
+#     def __init__(self,
+#                  name='Base',
+#                  size=1,
+#                  groups={
+#                          'RIG': ['C_main_GRP', 'C_global_CTL', 'C_globalGimbal_CTL', 'C_local_GRP'],
+#                          'GEO': ['C_main1_GRP', 'C_mainBuffer0_GRP'],
+#                          'SKELE': ['C_main2_GRP', 'C_mainBuffer2_GRP']
+#                          },
+#                 # TODO
+#                 #  colour: 1,
+#                 #  upaxis:'y',
+#                  ):
+#         """
+#
+#         :type children: list type
+#         """
+#         self.name = name
+#         self.size = size
+#         self.groups = groups
+#
+#         # TODO
+#         # self.colour = colour
+#         # self.upaxis = upaxis
+#
+#         self.fullName = 'C_' + name + '_GRP'
+#         # procs
+#         self.__create()
+#
+#     # def __instancecheck__(self, instance):
+#     #     if pm.objExist(self.name):
+#     #         logging.warning(self.finalFullName + "same name exists in scene.")
+#
+#     def __createStructure(self):
+#         # create the main group
+#         mainGrp = pm.group(em=True, n=self.name)
+#         # create the child groups
+#         for stuff in self.groups.keys():
+#             grp = pm.group(em=True, n=stuff, p=mainGrp)
+#
+#         # get values and make them in to groups
+#         for key, val in self.groups.items():
+#             nextList = []
+#             num = 0
+#             for lst in val:
+#                 if lst == val[0]:
+#                     pm.group(em=True, n=lst, p=key)
+#                     nextList.append(lst)
+#                 else:
+#                     if "CTL" in lst:
+#                         val = lst.replace('C_', '').replace('_CTL', '')
+#                         control = ControlCurves.controlCurves(name=val,
+#                                                               side='C',
+#                                                               shape='god',
+#                                                               num=0,
+#                                                               rotate=[0, 0, 0],
+#                                                               scale=self.size - num,
+#                                                               parent=[nextList[num]],
+#                                                               parentOrConst='parent',
+#                                                               hook=nextList[-1],
+#                                                               adjGrpNumber=1,
+#                                                               tag=True
+#                                                               )
+#                         print(control.getInstancesList()[-1])
+#                         nextList.append(control.getInstancesList()[-1])
+#                         num += 1
+#                     else:
+#                         bah = pm.group(em=True, n=lst, p=nextList[num])
+#                         nextList.append(bah)
+#                         num += 1
+#
+#     # Exacute process
+#     def __create(self):
+#         # Step 1
+#         self.__createStructure()
+#         # self.__createGrpsandSubGrps()
 
 class baseRig2():
     def __init__(self,
@@ -104,38 +105,36 @@ class baseRig2():
 
     def __createStructure(self):
         # create the main group
-        mainGrp = pm.group(em=True, n=self.name)
+        mainGrp = cmds.group(em=True, n=self.name)
         # create the child groups
-        for stuff in self.groups:
-            grp = pm.group(em=True, n=stuff, p=mainGrp)
+        for group in self.groups:
+            grp = cmds.group(em=True, n=group, p=mainGrp)
 
-        # RIG
-        rig = 'RIG'
-        RIG = ['C_main_GRP', 'C_global_CTL', 'C_globalGimbal_CTL', 'C_local_GRP']
+        main = cmds.group(em=True, n='C_main_GRP', p='RIG')
 
-        main = pm.group(em=True, n='C_main_GRP', p=rig)
         globalControl = ControlCurves.controlCurves(name='global',
-                                              side='C',
-                                              shape='global',
-                                              num=0,
-                                              rotate=[0, 0, 0],
-                                              scale=self.size,
-                                              hook = 'C_main_GRP',
-                                              parentOrConst='parent',
-                                              adjGrpNumber=1,
-                                              tag=True
-                                              )
-        num = self.size / 2.2
+                                                    side='C',
+                                                    shape='global',
+                                                    num=0,
+                                                    rotate=[0, 0, 0],
+                                                    scale=self.size,
+                                                    hook = main,
+                                                    # parent = ,
+                                                    parentOrConst='parent',
+                                                    adjGrpNumber=1,
+                                                    tag=True
+                                                    )
+
         godControl = ControlCurves.controlCurves(name='god',
                                               side='C',
                                               shape='god',
                                               num=0,
                                               rotate=[0, 0, 0],
-                                              scale=num,
+                                              scale= self.size / 2.2,
                                               hook = globalControl,
                                               parentOrConst='parent',
                                               adjGrpNumber=1,
                                               tag=True
                                               )
 
-        main = pm.group(em=True, n='C_local_GRP', p=godControl)
+        main = cmds.group(em=True, n='C_local_GRP', p=godControl)
