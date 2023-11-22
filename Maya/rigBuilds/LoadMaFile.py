eacd8e47f0410ba969dba129fb0020e1794f5ea4
rusherman00 <kimoon.t.song@gmail.com>
2023-11-21 23:04:15 -0500

    fix LoadMaFile

--- Maya/rigBuilds/LoadMaFile.py
+++ Maya/rigBuilds/LoadMaFile.py
@@ -1,13 +1,22 @@
 import pymel.core as pm
+import maya.cmds as cmds
+import importlib
 
-def load_ma_file(filepath):
+from rigBuilds import Checker
+importlib.reload(Checker)
+
+
+def loadMaFile(filepath):
     """
     Loads a Maya ASCII (.ma) file into the current Maya session.
     Returns True if the file was loaded successfully, False otherwise.
     """
+    paths = None
     try:
-        pm.importFile(filepath, type="mayaAscii", ignoreVersion=True)
+        bools, paths = Checker.checkIfFilePathsExist(filepath)
+        cmds.file(paths, i=True, type="mayaAscii", ignoreVersion=True, ra=True)
+
         return True
     except:
-        pm.warning("Failed to load file or path is not existing: {}".format(filepath))
+        cmds.warning("Failed to load file or path is not existing: {}".format(paths))
         return False
