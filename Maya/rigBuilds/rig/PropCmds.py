eb82a8f16e3276a3f705d0f157e4e48242300bdf
rusherman00 <kimoon.t.song@gmail.com>
2023-11-25 23:59:35 -0500

    WIP

--- Maya/rigBuilds/rig/PropCmds.py
+++ Maya/rigBuilds/rig/PropCmds.py
@@ -18,7 +18,8 @@ class propCmds():
                  controlSize=1,
                  controlRotation=[90, 0, 0],
                  parentControlsTo='C_god0_CNT',
-                 parentJointsGrpTo='SKELE',
+                 parentJointsTo='SKELE',
+
                  jointChain=True,
                  controlChain = True,
 
@@ -36,13 +37,14 @@ class propCmds():
         self.controlSize = controlSize
         self.controlRotation = controlRotation
         self.parentControlsTo = parentControlsTo
-        self.parentJointsGrpTo = parentJointsGrpTo
+        self.parentJointsTo = parentJointsTo
         self.jointChain = jointChain
         self.controlChain = controlChain
 
         # Vars
         self.fullName = '{}_{}'.format(side, name)
         self.jntList = []
+        self.controlList = None
 
         # initiate
         self.__create()
@@ -51,7 +53,7 @@ class propCmds():
     def __create(self):
         # main group for the joints
         self.mainRigGroup = cmds.group(n=self.fullName + "Joints_GRP", em=1,
-                                       parent=self.parentJointsGrpTo)
+                                       parent=self.parentJointsTo)
         cmds.select(clear=True)
 
         # create the joints
@@ -62,30 +64,57 @@ class propCmds():
                                                chain=self.jointChain,
                                                tag=True,
                                                )
+        # Figure how to create control chain
+        if self.controlChain:
+            print(f'creating for control chain')
+            hook = []
+            hook.append(self.parentControlsTo)
+            for num, parent in enumerate(self.jntList):
+                self.controlList = ControlCurves.controlCurves(name=self.name,
+                                                               side=self.side,
+                                                               num=num,
+                                                               shape=self.shape,
+                                                               rotate=self.controlRotation,
+                                                               scale=self.controlSize,
+                                                               parent=parent,
+                                                               parentOrConst='const',
+                                                               adjGrpNumber=1,
+                                                               hook=hook[-1],
+                                                               tag=True,
+                                                               )
+                hook.append(self.controlList.getControlName())
+                # self.controlList.getControlBase()
 
-        # create Curve controls
-        print(f'creating for {self.jntList}')
-        for num, parent in enumerate(self.jntList):
-            cnt = ControlCurves.controlCurves(name=self.name,
-                                              side=self.side,
-                                              num=num,
-                                              shape=self.shape,
-                                              rotate=self.controlRotation,
-                                              scale=self.controlSize,
-                                              parent=parent,
-                                              parentOrConst='const',
-                                              adjGrpNumber=1,
-                                              hook='C_god0_CNT',
-                                              tag=True,
-                                              )
-
-        # if self.controlChain:
-        #     print('doing joint chain')
-
+        else:
+            # create Curve controls
+            print(f'creating for {self.jntList}')
+            for num, parent in enumerate(self.jntList):
+                self.controlList = ControlCurves.controlCurves(name=self.name,
+                                                              side=self.side,
+                                                              num=num,
+                                                              shape=self.shape,
+                                                              rotate=self.controlRotation,
+                                                              scale=self.controlSize,
+                                                              parent=parent,
+                                                              parentOrConst='const',
+                                                              adjGrpNumber=1,
+                                                              hook=self.parentControlsTo,
+                                                              tag=True,
+                                                              )
 
     def __cleanUp(self):
         # remove locatorGuides Group
         cmds.delete(cmds.listRelatives(self.guideList[0], parent=True))
 
-        pass
+    def getControlName(self):
+        return self.controlList
 
+    # --- returns
+    def __str__(self):
+        return self.controlList
+
+    # def __repr__(self):
+    #     return self.finishedGrpLst[-1]
+
+    # def getInstancesList(self):
+    #     return self.finishedGrpLst
