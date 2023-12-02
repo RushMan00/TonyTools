4d638542fd5ba66b796b67411a45ec6ef5110728
rusherman00 <kimoon.t.song@gmail.com>
2023-12-02 00:50:37 -0500

    added more pram in createCurveOnNodes

--- Maya/rigBuilds/rig/CurveTools.py
+++ Maya/rigBuilds/rig/CurveTools.py
@@ -1,7 +1,10 @@
 import maya.cmds as cmds
 import pymel.core as pm
 
-def createCurveOnPoints(nodeList=[], name='C_curve0_CRV', parent='RIG'):
+def createCurveOnNodes(nodeList=[], name='C_curve0_CRV',
+                       parent='RIG', numberOfPoints=5,
+                       degree=3, end=True,
+                       ):
     '''
     createCurveFromNodes() Function
 
@@ -19,14 +22,27 @@ def createCurveOnPoints(nodeList=[], name='C_curve0_CRV', parent='RIG'):
     Date: 06/05/2023
     Version: 1.0.0
     '''
+    # checks
+    if numberOfPoints < 3:
+        cmds.warning('numberOfPoints must be more than 3')
+        return
 
-    # Get world space positions of the nodes and Create the curve
+    # Create Curve
     lst = [cmds.xform(point, query=True, translation=True, worldSpace=True) for point in nodeList]
     crv = cmds.curve(n=name, p=lst)
+
+    # Rename shapes
+    shapes = cmds.listRelatives(crv, shapes=True)
+    if shapes:
+        cmds.rename(shapes[0], name + 'Shape')
+
+    # Rebuild the curve
+    cmds.rebuildCurve(name, ch=0, rpo=1, rt=0, end=end, kr=0,
+                      kcp=0, kep=1, kt=0, s=numberOfPoints - 1,
+                      d=degree, tol=0.01)
 
     # Parent the curve if a parent is specified
     if parent:
         cmds.parent(crv, parent)
 
     return crv
-
